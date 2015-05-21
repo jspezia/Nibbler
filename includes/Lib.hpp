a8ed5827219340508d3dbf5067a824a781130bb3
ycribier <ycribier@student.42.fr>
2015-05-21 17:48:47 +0200

    rename libft->Lib, added handle_error and print_message methods

---
+++ includes/Lib.hpp
@@ -0,0 +1,14 @@
+#ifndef LIB_HPP
+# define LIB_HPP
+
+# include <string>
+
+# define IS_CRITIC 1
+
+bool	str_is_digit(char *str);
+
+void	handle_error(std::string msg, bool is_critic);
+void	print_message(std::string msg);
+
+#endif
+
