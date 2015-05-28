992697d0309e514d5de9c3b601f882bf3757f6fb
jspezia <jspezia@student.42.fr>
2015-05-28 17:27:11 +0200

    draw

--- exemples/SFML_2nd_test/sfmlLib_class.cpp
+++ exemples/SFML_2nd_test/sfmlLib_class.cpp
@@ -5,16 +5,18 @@ sfmlLib::sfmlLib(int h, int w, std::string name) :
 	_width(w),
 	_name(name)
 {
-	this->_window = (sf::RenderWindow *)new sf::Window(sf::VideoMode(h, w), name);
+	this->_window = new sf::RenderWindow(sf::VideoMode(h, w), name);
 }
 
 sfmlLib::~sfmlLib(void)
-{}
+{
+}
 
 void			sfmlLib::drawSomething(void) const
 {
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
 
     while (this->_window->isOpen())
     {
@@ -26,29 +28,10 @@ void			sfmlLib::drawSomething(void) const
                 this->_window->close();
         }
 
-        // this->_window->clear();
-        // this->_window->draw(shape);
+        this->_window->clear();
+        this->_window->draw(shape);
         this->_window->display();
     }
-
-    
-    // sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    // while (window.isOpen())
-    // {
-
-    //     sf::Event event;
-    //     while (window.pollEvent(event))
-    //     {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
-
-    //     window.clear();
-    //     window.draw(shape);
-    //     window.display();
-    // }
-
-
 }
 
 sfmlLib			*createWindow(const int h, const int w, const std::string &name)
